509aaa5a10d1a37a53a4c98f09bb3ef38d104bd8
nabaraj basnet <nabarajbasnet2000@gmail.com>
2025-01-20 22:26:31 +0545

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.screen.availWidth / 1.2;
+canvas.height = window.screen.availHeight / 1.6;
+canvas.style.backgroundColor = 'gray';
