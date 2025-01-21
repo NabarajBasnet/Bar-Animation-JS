2820f36b665c1c6053e62ebb0423fb18c1045408
nabaraj basnet <nabarajbasnet2000@gmail.com>
2025-01-21 11:22:42 +0545

    Basic changes

--- script.js
+++ script.js
@@ -2,20 +2,23 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 import Bars from "./Bars.js";
 
+let barWidth = 10;
+let barHeight = 10;
+
+const bars = Array.from({ length: 20 }, () => {
+    return new Bars(10, 10, barWidth, barHeight);
+});
+
 const resizeCanvas = () => {
 
     canvas.width = document.documentElement.clientWidth / 1.2;
     canvas.height = document.documentElement.clientHeight / 1.6;
-    canvas.style.backgroundColor = 'gray';
+    canvas.style.border = '1px solid gray';
 
     requestAnimationFrame(resizeCanvas);
 };
 
 requestAnimationFrame(resizeCanvas);
-
-const bars = Array.from({ length: 20 }, () => {
-    return new Bars(10, 10, 10, 10);
-});
 
 const gameLoop = () => {
     bars.forEach((bar) => bar.update(ctx));
