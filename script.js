95d863b4745cf0f5ddee9225f38da980665ae131
nabaraj basnet <nabarajbasnet2000@gmail.com>
2025-01-21 07:40:57 +0545

    Adding responsiveness in canvas

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 import Bars from "./Bars.js";
 
-canvas.width = window.screen.availWidth / 1.2;
-canvas.height = window.screen.availHeight / 1.6;
-canvas.style.backgroundColor = 'gray';
+const resizeCanvas = () => {
+
+    canvas.width = document.documentElement.clientWidth / 1.2;
+    canvas.height = document.documentElement.clientHeight / 1.6;
+    canvas.style.backgroundColor = 'gray';
+
+    requestAnimationFrame(resizeCanvas);
+};
+
+requestAnimationFrame(resizeCanvas);
 
 const bars = Array.from({ length: 20 }, () => {
-    return new Bars(10,10,10,10);
-})
+    return new Bars(10, 10, 10, 10);
+});
 
 const gameLoop = () => {
     bars.forEach((bar) => bar.update(ctx));
@@ -17,3 +24,4 @@ const gameLoop = () => {
 }
 
 requestAnimationFrame(gameLoop);
+
